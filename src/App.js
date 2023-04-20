5dbe6cdbdc47304e369f05a67df2bf427e719447
Roshan Panta <roshanpanta2059@gmail.com>
2023-04-20 18:41:15 +0545

    done upto wrapper component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import Contact from "./components/Contact";
 import Header from "./components/Header";
+import Wrapper from "./Wrapper";
 import "./styles/app.css";
 const contacts = [
   { name: "Roshan", contact: 988784545, add: "manamaiju" },
-  { name: "Andrew", contact: 988784545, add: "manamaiju" },
-  { name: "Leonard", contact: 988784545, add: "manamaiju" },
+  { name: "Andrew", contact: 988784545, add: "newzealand" },
+  { name: "Leonard", contact: 988784545, add: "Ireland" },
+  { name: "Sudip", contact: 983843344, add: "koteshwor" },
 ];
 const App = () => {
   return (
     <>
       <Header />
-      <div className="App">
-        <h2>
-          <Contact data={contacts[0]} />
-          <Contact data={contacts[1]} />
-          <Contact data={contacts[2]} />
-        </h2>
-      </div>
+      <Wrapper>
+        <div className="App">
+          <h2>
+            <Contact data={contacts[0]} />
+            <Contact data={contacts[1]} />
+            <Contact data={contacts[2]} />
+            <Contact data={contacts[3]} />
+          </h2>
+        </div>
+      </Wrapper>
     </>
   );
 };
